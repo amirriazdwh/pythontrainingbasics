fe56f8f8c2bdc6e40cd13eeb07686b6aa541e58c
amirr <amir.riaz.dwh@gmail.com>
2021-10-25 17:47:49 +0400

    stage three files

---
+++ python/1.5.  underscore meaning.py
@@ -0,0 +1,9 @@
+"""
+Single Leading Underscore _var: Naming convention indicating name is meant for internal use. A hint for programmers and not enforced by programmers.
+Double Leading Underscore __var: Triggers name mangling when used in class context. Enforced by the Python interpreter.
+Single Trailing Underscore var_: Used by convention to avoid naming conflicts with Python keywords.
+Double Trailing Underscore __var__: Indicates special methods defined by Python language.
+Underscore _: Used as a name for temporary variables.
+Python Features
+
+"""
