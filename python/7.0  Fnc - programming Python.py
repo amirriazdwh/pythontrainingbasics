32380c050241a2d37b891bd7c2e347af08cd3bc3
amirr <amir.riaz.dwh@gmail.com>
2021-10-29 20:32:46 +0400

    stage three files

--- python/7.0  Fnc - programming Python.py
+++ python/7.0  Fnc - programming Python.py
@@ -9,3 +9,13 @@ in functions the values of argument is immutable.  the logic inside the fuction 
      functions can be used as expression.
      function can be defined and run inside function
 """
+import functools
+
+items =[1,2,3,4,5,6,7,8,9]
+total = functools.reduce(lambda a, b: a+b, items)
+print(total)
+
+totallist =list(map(lambda b: b*2 ,items))
+print(totallist)
+
+list(filter(lambda x: x > 100, [1, 111, 2, 222, 3, 333]))
