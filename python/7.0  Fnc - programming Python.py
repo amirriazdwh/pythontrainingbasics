6cb617bb254222782112c3a26128d09cb8f6921c
amirr <amir.riaz.dwh@gmail.com>
2021-10-29 21:30:30 +0400

    stage three files

--- python/7.0  Fnc - programming Python.py
+++ python/7.0  Fnc - programming Python.py
@@ -24,15 +24,33 @@ double = lambda x: x * 2
 
 print(double(5))
 
-
-
 import functools
 
-items =[1,2,3,4,5,6,7,8,9]
-total = functools.reduce(lambda a, b: a+b, items)
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+total = functools.reduce(lambda a, b: a + b, items)
 print(total)
 
-totallist =list(map(lambda b: b*2 ,items))
+totallist = list(map(lambda b: b * 2, items))
 print(totallist)
 
 list(filter(lambda x: x > 100, [1, 111, 2, 222, 3, 333]))
+
+
+def add_num(message):
+    def add_inc(increment, numbers):
+        message()
+        incremented_nums = []
+        for n in numbers:
+            incremented_nums.append(n + increment)
+        return incremented_nums
+
+    return add_inc
+
+@add_num
+def message1():
+    print("Doing something...")
+
+
+#message1 = add_num(message1)
+print(message1(5, [28, 93]))
+
