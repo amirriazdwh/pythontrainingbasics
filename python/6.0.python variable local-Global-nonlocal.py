742983133392885a4526af2402c33f020b040b96
amirr <amir.riaz.dwh@gmail.com>
2021-10-31 10:50:59 +0400

    stage three files

--- python/6.0.python variable local-Global-nonlocal.py
+++ python/6.0.python variable local-Global-nonlocal.py
@@ -1,17 +1,81 @@
-print(ord('c'))
-print(str(99))
+"""
+Global Variables
+In Python, a variable declared outside of the function or in global scope is known as a global variable. This means that a global variable can be accessed inside or outside of the function.
 
-ls = []
-ls=list()
-ls.append(1)
-ls.append(2)
+Let's see an example of how a global variable is created in Python.
 
-print(ls)
+Example 1: Create a Global Variable
 
-str0 ="abcd"
-lst = list(str0)
-print(lst)
+"""
+x = "global"
 
-str1="".join(lst)
-print(str1)
+def foo():
+    print("x inside:", x)
+
+
+foo()
+print("x outside:", x)
+
+"""
+In the above code, we created x as a global variable and defined a foo() to print the global variable x. Finally, we call the foo() 
+which will print the value of x.
+
+What if you want to change the value of x inside a function?
+"""
+
+x = "global"
+
+def foo():
+    x = x * 2
+    print(x)
+
+foo()
+
+"""
+The output shows an error because Python treats x as a local variable and x is also not defined inside foo().
+
+To make this work, we use the global keyword. Visit Python Global Keyword to learn more.
+"""
+
+"""
+Local Variables
+A variable declared inside the function's body or in the local scope is known as a local variable.
+
+Example 2: Accessing local variable outside the scope
+"""
+def foo():
+    y = "local"
+
+
+foo()
+print(y)
+
+"""
+Example 3: Create a Local Variable
+Normally, we declare a variable inside the function to create a local variable.
+Let's take a look at the earlier problem where x was a global variable and we wanted to modify x inside foo().
+"""
+
+def foo():
+    y = "local"
+    print(y)
+
+foo()
+
+"""
+Global and local variables
+Here, we will show how to use global variables and local variables in the same code.
+
+Example 4: Using Global and Local variables in the same code
+"""
+x = "global "
+
+def foo():
+    global x
+    y = "local"
+    x = x * 2
+    print(x)
+    print(y)
+
+foo()
 
