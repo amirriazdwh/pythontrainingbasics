109de159f3ff206f0064492f4b88d07ed6b01a4e
amirr <amir.riaz.dwh@gmail.com>
2021-10-31 10:52:34 +0400

    stage three files

--- python/6.0.python variable local-Global-nonlocal.py
+++ python/6.0.python variable local-Global-nonlocal.py
@@ -79,3 +79,23 @@ def foo():
 
 foo()
 
+"""
+In the above code, we declare x as a global and y as a local variable in the foo(). Then, we use multiplication operator * to modify
+ the global variable x and we print both x and y.
+
+After calling the foo(), the value of x becomes global global because we used the x * 2 to print two times global. 
+After that, we print the value of local variable y i.e local
+"""
+
+x = 5
+
+def foo():
+    x = 10
+    print("local x:", x)
+
+
+foo()
+print("global x:", x)
+
+
+
