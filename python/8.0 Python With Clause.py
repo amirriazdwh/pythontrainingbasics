5f7754fe4e7942b31accba0c7b201931365e1e59
amirr <amir.riaz.dwh@gmail.com>
2021-11-07 18:09:28 +0400

    stage three files

--- python/8.0 Python With Clause.py
+++ python/8.0 Python With Clause.py
@@ -2,6 +2,10 @@
 with statement helps in simplifying some common resource management patterns by abstracting their functionally and
 allowing them to be factored out and reused.
 
+it is used to manage the safe acquistion and release of system resources.   resource are acquired by the with statement
+and release when the execution leaves the with context.  since its an alternative form of try/catch statement and relies
+on context to release.  its called context manager.
+
 A good way to see this feature used effectively is looking at examples in the python standard library.  The build-in
 open() function provides us with an excellent use case:
 
@@ -9,6 +13,21 @@ open() function provides us with an excellent use case:
 # opens the file for write and writes hello world
 with open('D:\python_data\hello_file.txt', 'w') as f:
     f.write("hello world")
+
+"""
+with clause creates an object and when the object is initialized,  in __init__ method which takes function 
+with then calls __enter__ method and return the handle
+ 
+
+f = with(open('D:\python_data\hello_file.txt', 'w') )
+f.write ("hello world")
+
+# it does not find further calls of f.  it considers that context of f is over and call
+del f 
+
+which call destructor and call the __exit__ method which closes the file handlers. 
+
+"""
 
 """
 opening the files using the with stateemnt is generally recommended because it ensure that open file descriptor is close
