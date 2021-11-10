9d27979e9477bc03e12d2197ede49a89fa8e485f
amirr <amir.riaz.dwh@gmail.com>
2021-11-10 11:36:54 +0400

    stage three files

--- python/1.6 String slicing[].py
+++ python/1.6 String slicing[].py
@@ -3,9 +3,30 @@ Sequences also support slicing: a[i:j] selects all items with index k such that 
 a slice is a sequence of the same type. This implies that the index set is renumbered so that it starts at 0.
 
 you can find the slice of string by following formula
-if k is the index of the array then index slice for that element will be
+if k is the index of the sequence then index slice for that element will be
 
-(i=k)=<k<=(j=k+1)  where i is the slice starting point and j is the slice ending point.  k is array index.  so if we want
+(i=k)=<k<=(j=k+1)  where i is the slice starting point and is equal to index k and j is the slice ending point and is equal
+to k+1.
+
+an index will return the single element without sequence notation given (means list, tuple, dict will not be mentioned)
+an slice will give output as a list , tuple or sequence  etc.   for example.
+
+S = 'ABCDEFGHI'
+S[0] = A
+
+while S[0:3] ="ABCD"   # here Slice is returning sequence.  since a slice return a sequence therefore it must have a
+upper bound and lower bound.  so the sytax of slice is:
+
+S[Start: End: Step] so for S
+      0  1     2   3   4   5   6   7    8   9
+S = '  A    B    C   D   E   F   G   H   I'
+    -9  -8    -7  -6  -5  -4  -3  -2  -1
+
+a index k always start with 0.
+
+a positive slice always start with 0 as lower bound and a finite number as upper bound ,  9 in this case.
+
+k is array index.  so if we want
 to get array for 0 to 6.    the starting slice is lower bound and ending slice is upper bound.
 i=0 and j=6+1=7 so
 
@@ -39,6 +60,8 @@ print(S[6::-2])   #GECA
 print(S[-7:-1:2])  # CEG    -5 -3 -2 0   k>j stop
 print(S[-7::2])  # CEG    -5 -3 -2 0   k>j stop
 print(S[0:7])	# ABCDEFG
+
+print(S[-8:])	# ABCDEFG
 
 
 print(S[0:1])	# CDEFG  0=<0<1
