7cde2ccb3208966231f9b0957e2244a9823022de
amirr <amir.riaz.dwh@gmail.com>
2021-11-11 00:37:51 +0400

    stage three files

--- python/1.6 String slicing[].py
+++ python/1.6 String slicing[].py
@@ -2,56 +2,40 @@
 Sequences also support slicing: a[i:j] selects all items with index k such that i <= k < j. When used as an expression,
 a slice is a sequence of the same type. This implies that the index set is renumbered so that it starts at 0.
 
-you can find the slice of string by following formula
-if k is the index of the sequence then index slice for that element will be
-
 (i=k)=<k<=(j=k+1)  where i is the slice starting point and is equal to index k and j is the slice ending point and is equal
 to k+1.
 
-an index will return the single element without sequence notation given (means list, tuple, dict will not be mentioned)
-an slice will give output as a list , tuple or sequence  etc.   for example.
+Rules  -- Above definitiion sets the following rules for slice[Start(i): End(j) : Step]
+-------
+1.  "Start" describes the start value from where string has to be slice.  default is from begining.
+2.  "Stop"  describes the value where we have to stop. the result will not include the stopping value.  to include the
+      stop values you have to move next index thats why we mathematically represent it as   i<=k<j
+3.   "Step" describe the direction and increment or decrement size.  in case of +ive its a straight and in of -ive it will
+      reverse.
+4.    an index will return the single element without sequence slicing notation means S[0] = A
+5.    an slice notation will give output as a list , tuple or sequence  etc.   for example. S[0:1]=[]
+6.    please note that start is not the lower bound and end is not always the upper bound.  they are just values whose direction
+      is controlled by step.
 
-S = 'ABCDEFGHI'
-S[0] = A
 
-while S[0:3] ="ABCD"   # here Slice is returning sequence.  since a slice return a sequence therefore it must have a
-upper bound and lower bound.  so the sytax of slice is:
+the indexes are arranged as under:
 
 S[Start: End: Step] so for S
-      0  1     2   3   4   5   6   7    8   9
+-----------------------------
+       0    1    2   3   4   5   6   7   8
 S = '  A    B    C   D   E   F   G   H   I'
-    -9  -8    -7  -6  -5  -4  -3  -2  -1
+      -9   -8   -7  -6  -5  -4  -3  -2  -1
 
-a index k always start with 0.
+negative number are achieved as:  k-len(S).   thats why  8-9 =-1  we
+Using the above formula slicing system can convert -ive start and stop value +ive values.  but it cannot change the
+Step value.
 
-a positive slice always start with 0 as lower bound and a finite number as upper bound ,  9 in this case.
-
-k is array index.  so if we want
-to get array for 0 to 6.    the starting slice is lower bound and ending slice is upper bound.
-i=0 and j=6+1=7 so
-
-Some sequences also support “extended slicing” with a third “step” parameter: a[i:j:k] selects all items of a with index x
-where x = i + n*k, n >= 0 and i <= x < j
-
-in case k is index between 0 and 6 and we want slice array from 0 to 7  i=0 , n=2 and j=7
-0<=0<1   0+2=2       i+n=x ( next x)  A
-2<=2<3   2+2=4                        C
-4<=4<5                                E
-6<=6<7                                G
-            +
-in case index k is from 6 to 0 (k) and n =-2   a[6:0:2]
-
-6    6<=6<7     G    k-n  6-2=k
-4    4<=4<5     E         4-2=k
-2    2<=2<3     C         2-2=k =0   when k=<j stop
-0    0<=0<1     A
------------------------------------
-in case the k is from -1 to -6
 
 """
 
 
 S = 'ABCDEFGHI'
+print(len(S))
 print(S[2:7])	   #CDEFG
 print(S[0:7:2])	   #ACEG
 print(S[0:6:2])	   #ACEG
@@ -63,3 +47,9 @@ print(S[0:7])	# ABCDEFG
 
 print("################")
 print(S[-5::-1])	# EDCBA
+print(S[4::-1])	    # EDCBA
+
+# Example of list,  how index and slice change the output.
+num =[10, 20, 30]
+print(num[0:1])
+print(num[0])
