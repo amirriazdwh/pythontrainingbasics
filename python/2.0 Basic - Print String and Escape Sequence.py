1d59d28f3a96bfa3b57e3d410cd781c720c4e6a6
amirr <amir.riaz.dwh@gmail.com>
2021-11-15 15:30:50 +0400

    stage three files

--- python/2.0 Basic - Print String and Escape Sequence.py
+++ python/2.0 Basic - Print String and Escape Sequence.py
@@ -1,7 +1,23 @@
 # is comments in python
+
 """
-for muliple line comments we use 3 quotations
-this is multiline comments
+String formatting method in python.
+1. Old style: "Hello %s" %name   - % means replace the latter after s with name variable.   in case of multiple variable
+   you can use "Hello Mr %s with %s "%(Firstname, LastName).  s means string.  other formats are specified below.
+2. New Style:.   "Hello, {}".format(name)
+   Hello {name},  there is a 0x{errorno:x} error!. format(name=name, errorno=errno)
+3. Literal String interpolation:   f'Hello {name}'.  this is actually a sugar coat version of 'Hello {name}'.format(name)
+   here f is equivalent to format function and variable is inside {name}
+4. Function Style:  "Hello"+name+"!"
+5. Template Strings:  "Hey, $name!"
+     from string import Template
+     t=Template('Hey, $name!')
+     t.substitute(name=name)
+
+"""
+
+
+"""
 d	Signed integer decimal.
 i	Signed integer decimal.
 o	Unsigned octal.
@@ -25,32 +41,79 @@ s	String (converts any python object using str()).
  	If no sign (minus sign e.g.) is going to be written, a blank space is inserted before the value.
 +	A sign character ("+" or "-") will precede the conversion (overrides a "space" flag).
 """
+"""
+in case of print statement
+d means digits...   2d say digit will have 2 digits
+f means fraction... 5.3f says fraction will have 5 digits and 3 will be fraction part and 2 will be digits parts
+% means replace values by order.  if %2d and %3d and values are 23 and 25.  then they will be printed in order
+"""
 
+
+# print method argument are of type *argu,  so multiple values can be given as tuple
+print("this is first line", "this is second line")
+
+# print(self, *args, sep=' ', end='\n', file=None):
+#
+# multiple variable example
 q=458
 p=0.586
 print(q, p, p * q, sep=",")
 
-print("this is first line", "this is second line")
+print("this", "is", "the", "line of code ", sep=" ", end="\n\n")
 
 # by default the end character is new line
 print("this is the line of code ", end=" ")
 print(" this is second line ")
+
 """
-in case of print statement
-d means digits...   2d say digit will have 2 digits
-f means fraction... 5.3f says fraction will have five digits and 3 will be fraction part and 2 will be digits parts
-% means replace values by order.  if %2d and %3d and values are 23 and 25.  then they will be printed in order
+----------------------------------------
+Method #1
+----------------------------------------
 """
+
 print("Geeks : % 2d, Portal : % 5.1f" %(1, 05.333))
 print("\n")
 # escape sequence are \n \r
-#    %[flags][width][.precision]type
+# edit format is : %[flags]5[width]3[.precision]ftype  example,  so its %5.3d
 print("c:\\narry")
 print("Total students : % 3d, Boys : % 2d" %(240, 120))
 print("% 7.3o"% (25))
 print("% 10.3E"% (356.08977))
 
+
+"""
+----------------------------------------
+Method #2
+----------------------------------------
+"""
+
 print("First argument: {0}, second one: {1}".format(47,11))
-# 0 is first position and 1 is the second position and : gives precision
+# 0 is first position and 1 is the second position and : gives precision,  you can specify position  position:flag:type.
 print("Second argument: {1:3d}, first one: {0:7d}".format(47,11))
+
+
+"""
+----------------------------------------
+Method #3
+----------------------------------------
+"""
+print(f"Second argument: {q}, first one: {p}, product  {p*q}")
+
+"""
+----------------------------------------
+Method #5
+----------------------------------------
+"""
+
+
+from string import Template
+name="Amir"
+t=Template('Hey, $name!')
+str0=t.substitute(name=name)
+print(str0)
+
+"""
+Note:  this will not work in python
+"""
+print("My name is $name")
 
