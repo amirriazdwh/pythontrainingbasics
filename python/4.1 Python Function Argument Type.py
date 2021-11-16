dee5c1c76f1bcb520e0c93af3857ddfe84797c3a
amirr <amir.riaz.dwh@gmail.com>
2021-11-16 20:59:23 +0400

    stage three files

---
+++ python/4.1 Python Function Argument Type.py
@@ -0,0 +1,10 @@
+"""
+1.  the argument inside function are called formal arguments
+2.   the value which are passed to function are called actual arguments
+3.  arguments are passed to functions as reference.  They are not passed by values.
+4.  there are four types of function Arguments in python.
+    a.  positional argument
+    b.  key work
+    c.  default
+    d.  variable word
+"""
