e11c3977000abd1aaab7800b59b9dc034603c125
amirr <amir.riaz.dwh@gmail.com>
2022-03-14 16:40:14 +0400

    stage three files

--- python/executor/execute.sh
+++ python/executor/execute.sh
@@ -1,3 +1,3 @@
 temp_path=$(realpath $0)
 path=${temp_path%/*}
-/usr/hdp/2.6.5.0-292/spark2/bin/spark-submit --master yarn --driver-memory 20G --executor-memory 10G --num-executors 10 --executor-cores 4 $path/solution.py all all
+/usr/hdp/2.6.5.0-292/spark2/bin/spark-submit --master yarn --driver-memory 8G --executor-memory 8G --num-executors 10 --executor-cores 4 $path/solution.py all all
