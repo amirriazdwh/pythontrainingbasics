42562933b5a2d8170a89bb9d7dea40ab4b7aa0e3
amirr <amir.riaz.dwh@gmail.com>
2022-03-24 01:21:02 +0400

    stage three files

--- python/0.2 Py Arch.-language-Functions.py
+++ python/0.2 Py Arch.-language-Functions.py
@@ -642,6 +642,27 @@ print ("tuple:{0}".format(test_args_kargs(1,2,3, 4)))
 print ("None:{0}".format(test_args_kargs()))
 
 
+"""
+32. Lambda functions 
+
+Small anonymous functions can be created with the lambda keyword. This function returns the sum of its two arguments: lambda a, b: a+b. Lambda
+ functions can be used wherever function objects are required. They are syntactically restricted to a single expression. Semantically, they are just syntactic 
+ sugar for a normal function definition. Like nested function definitions, lambda functions can reference variables from the containing scope:
+"""
+
+def make_incrementor(n):
+    return lambda x: x + n
+
+f = make_incrementor(42)
+f(0)
+
+f(1)
+
+pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+pairs.sort(key=lambda pair: pair[1])
+pairs
+
+
 
 
 
