0d6b8cf889a0f1bf51759cdc03c4274d5197fd68
amirriazdwh <amir.riaz.dwh@gmail.com>
2024-09-16 22:37:39 +0400

    long time commit

---
+++ python/unixcommand.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def run_unix_cmd(args_list, output_file='output.txt', error_file='error.txt'):
+    """
+    Run a Unix command and write output and error to files.
+    """
+    print('Running system command: {0}'.format(' '.join(args_list)))
+
+    with open(output_file, 'w') as out, open(error_file, 'w') as err:
+        proc = subprocess.Popen(args_list, stdout=out, stderr=err, universal_newlines=True)
+        proc.communicate()
+
+    return proc.returncode
