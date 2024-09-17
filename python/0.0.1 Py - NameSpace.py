628eb44620e872164dd00830e599a17014b8510c
amirriazdwh <amir.riaz.dwh@gmail.com>
2024-09-17 20:28:26 +0400

    p  =[c6yt

--- python/0.0.1 Py - NameSpace.py
+++ python/0.0.1 Py - NameSpace.py
@@ -4,18 +4,22 @@ Key Points to Remember- Namespace
 ############################################################################
 python has 4 types of namespace.
 1. Built-in Namespace:
-Initialized when the Python interpreter starts. it loads the buildin module of python builtin.py and other moduel to provide acess to print functions etc
+Initialized when the Python interpreter starts. it loads the buildin module of python builtin.py and other modules to provide access
+to builtin function for example print functions etc
 
 2. Global Namespace:
-When a module or Python file is loaded, it is initialized and registered in sys.modules. This registration helps the program find modules that
+When a module or Python file is loaded, it is initialized into module and registered in sys.modules. This registration helps the program find modules that
 have already been loaded. A global namespace is created for each module that is loaded and registered in sys.modules. This global namespace
 contains the module’s variables, classes, and functions. Each module has its own namespace.
 When mymodule is imported, it creates its own global namespace containing global_var and greet. If you create a global variable
 inside a function using the global keyword, it gets created in the module’s global namespace.
 Using del mymodule deletes the reference to the module, effectively removing its global namespace and the global variables within the module.
 After deletion, attempting to access mymodule raises a NameError because the module is no longer defined.
+so a global variable has its global scope inside module only
 
 Note: Module functions and variables are defined in the module’s global namespace.
+      Within a Module: Global variables are accessible only within the module they are defined in.
+      Across Modules: You can share global variables across modules by defining them in a separate module and importing that module wherever needed.
 
 3. Enclosing Namespace:
 Created dynamically when a nested function is called.
