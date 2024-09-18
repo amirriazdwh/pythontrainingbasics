fa310d5cd1aa445eaac63234a68da34fab0aea5c
amirriazdwh <amir.riaz.dwh@gmail.com>
2024-09-18 22:42:53 +0400

    p  =[c6yt

---
+++ python/0.0.6 Python Modules.py
@@ -0,0 +1,45 @@
+"""
+In Python, the import and from ... import statements are used to include code from one module into another
+. Here’s a breakdown of how they work and their differences:
+
+import math
+print(math.sqrt(16))  # Output: 4.0
+
+Usage: You need to prefix the module name (math) to access its functions or variables.
+Scope: Imports the entire module, making all its functions and variables available.
+
+from math import sqrt
+print(sqrt(16))  # Output: 4.0
+
+Usage: You can directly use the imported function (sqrt) without prefixing it with the module name.
+Scope: Imports only the specified functions, classes, or variables, not the entire module.
+
+
+from ... import *: Imports all public names from a module.
+Namespace Pollution: Can lead to name conflicts and harder-to-read code.
+__all__ Attribute: Controls what gets imported with import *.
+
+# math_utils.py
+__all__ = ['PI', 'add']
+
+PI = 3.14159
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def _private_function():
+    pass
+
+
+from math_utils import *
+
+print(PI)          # Output: 3.14159
+print(add(2, 3))   # Output: 5
+# print(subtract(5, 2))  # This will raise an error
+
+"""
+
+
