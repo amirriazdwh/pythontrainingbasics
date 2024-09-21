f5ff358a63088bc337f00f2090c36682bab471e8
amirriazdwh <amir.riaz.dwh@gmail.com>
2024-09-21 16:02:46 +0400

    p  =[c6yt

---
+++ python/0.0.0 Python Keyword sytax reasons.py
@@ -0,0 +1,16 @@
+"""
+1.  :    define the start of programming block.  in java this is done by { }
+    in python : define the start of programming block.
+    indentation define the scope of the block
+    end of indentation define end of this block.
+
+for example:
+
+if condition:
+    # This is the start of the block
+    print("Condition is true")
+    # This is still part of the block
+print("This is outside the block")  # Indentation level decreased, so this is outside the block
+
+
+"""
