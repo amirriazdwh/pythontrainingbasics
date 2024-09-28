edcf815cc3a0232d5e71d8ed3e89434ff2c374ef
amirriazdwh <amir.riaz.dwh@gmail.com>
2024-09-28 17:06:01 +0400

    p  =[c6yt

--- python/0.0.2 - brackets meaning and usage.py
+++ python/0.0.2 - brackets meaning and usage.py
@@ -4,7 +4,7 @@ Brackets and their Uses in Python:
 ################################################
 
 Square Brackets []:
-
+--------------------
 Lists: 
 These are mutable, ordered collections. Example: [], [1, 2, 3], [i**2 for i in range(5)]. list indexes are immutable 
 as they are variable to a function. 
@@ -20,14 +20,21 @@ Getting a part of a sequence. Example: 'abc'[:2] → 'ab'.
 
 
 Parentheses ():
-
+-------------------
 Tuples: 
-These are immutable, ordered collections. Example: (), (1, 2, 3). You can also create a tuple without parentheses like this: t = 1, 2 results in (1, 2).
-Order of Operations: Used to clarify the precedence of mathematical operations. Example: (n-1)**2.
+These are immutable, ordered collections. Example: (), (1, 2, 3). You can also create a tuple without
+parentheses like this: t = 1, 2 results in (1, 2).
+
+Order of Operations:
+Used to clarify the precedence of mathematical operations. Example: (n-1)**2.
+
 Generator Expressions: A concise way to create iterators. Example: (i**2 for i in range(5)).
-Function Calls: Used to call functions or methods. Example: print(), int(), range(5). You can also use generator expressions within function calls: sum(i**2 for i in range(5)).
+
+Function Calls: Used to call functions or methods. Example: print(), int(), range(5). You can also use
+generator expressions within function calls: sum(i**2 for i in range(5)).
 
 Keyobject:
+--------------
 in python an object define the key value pair.  its basic object for creating dictionary
 
 Curly Braces {}:
