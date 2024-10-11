7aaa022f49401ffc0e7aac6120dd7fc73b9d7cc4
amirriazdwh <amir.riaz.dwh@gmail.com>
2024-10-11 19:21:35 +0400

    -

--- python/2.6. Iterator and Generator details.py
+++ python/2.6. Iterator and Generator details.py
@@ -175,3 +175,49 @@ yes_expr = ('yes' for _ in range(stop))
 # Finally, note that generators provide even more functionality as coroutines. I explain Generators, along with the yield statement,
 # in depth on my answer to "What does the “yield” keyword do?".
 """
+
+iterable_value = 'FacingIssuesOnIT'
+iterable_obj = iter(iterable_value)
+
+while True:
+   try:
+   # Iterate by calling next
+      item = next(iterable_obj)
+      print(item)
+   except StopIteration as err:
+      print('Stop Iteration occured', err)
+      break
+
+
+   class PrintNumber:
+      def __init__(self, max):
+         self.max = max
+
+      # an iter always return self
+      def __iter__(self):
+         self.num = 0
+         return self
+
+      # a next always return self.value
+      def __next__(self):
+         if (self.num >= self.max):
+            raise StopIteration
+         self.num += 1
+         return self.num
+
+
+printNum = PrintNumber(3)
+iterable_obj1 = iter(printNum)
+
+while True:
+   try:
+   # Iterate by calling next
+      item = next(iterable_obj1)
+      print(item)
+   except StopIteration as err:
+      print('Stop Iteration occured', err)
+      break
+   except RuntimeError as err:
+      print("The value returned from the StopIteration statement is:", err)
+
+
